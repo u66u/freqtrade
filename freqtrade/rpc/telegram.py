66cdf7044d58c8d4978cf00ce130bebf33c582bb
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2022-01-20 14:54:23 +0900

    Update telegram.py

--- freqtrade/rpc/telegram.py
+++ freqtrade/rpc/telegram.py
@@ -438,7 +438,7 @@ class Telegram(RPCHandler):
                         if x == 0:
                             lines.append("  ")
                             lines.append("*Buy #"+str(x+1)+":*")
-                            lines.append("*Buy Amount:* {:.8f}".format(r['filled_buys'][x].amount)+" ({0} {1})".format(r['filled_buys'][x].cost, r['base_currency']))
+                            lines.append("*Buy Amount:* {:.8f} ({:.8f} {})".format(r['filled_buys'][x].amount, r['filled_buys'][x].cost, r['base_currency']))
                             lines.append("*Average Buy Price:* {:.8f}".format(r['filled_buys'][x].average))
                         else:
                             lines.append("  ")
@@ -452,7 +452,7 @@ class Telegram(RPCHandler):
                             minus_on_buy = (r['filled_buys'][x].average - prev_avg_price)/prev_avg_price
                             lines.append("*Buy #"+str(x+1)+":* ("+str(arrow.get(r['filled_buys'][x].order_filled_date).humanize())+
                                          ", at {:.2%} avg profit)".format(minus_on_buy))
-                            lines.append("*Buy Amount:* {:.8f}".format(r['filled_buys'][x].amount)+" ({0} {1})".format(r['filled_buys'][x].cost, r['base_currency']))
+                            lines.append("*Buy Amount:* {:.8f} ({:.8f} {})".format(r['filled_buys'][x].amount, r['filled_buys'][x].cost, r['base_currency']))
                             lines.append("*Average Buy Price:* {:.8f} ({:.2%} from 1st buy rate)".format(r['filled_buys'][x].average, price_to_1st_buy))
 
                 # Filter empty lines using list-comprehension
