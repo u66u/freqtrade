884ac04df0b5a81bb57900300aedc857a7d7c055
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2022-12-13 13:56:18 +0900

    new hyperopt loss

---
+++ user_data/hyperopts/ExpectancyAvgProfitLoss.py
@@ -0,0 +1,44 @@
+"""
+MaxDrawDownHyperOptLoss
+
+This module defines the alternative HyperOptLoss class which can be used for
+Hyperoptimization.
+"""
+from datetime import datetime
+
+from pandas import DataFrame
+
+from freqtrade.data.metrics import calculate_expectancy
+from freqtrade.optimize.hyperopt import IHyperOptLoss
+
+
+class ExpectancyAvgProfitLoss(IHyperOptLoss):
+
+    """
+    Defines the loss function for hyperopt.
+
+    This implementation optimizes for max draw down and profit
+    Less max drawdown more profit -> Lower return value
+    """
+
+    @staticmethod
+    def hyperopt_loss_function(results: DataFrame, trade_count: int,
+                               min_date: datetime, max_date: datetime,
+                               *args, **kwargs) -> float:
+
+        """
+        Objective function.
+
+        Uses profit ratio weighted max_drawdown when drawdown is available.
+        Otherwise directly optimizes profit ratio.
+        """
+        # total_profit = results['profit_abs'].sum()
+
+        average_profit = results['profit_ratio'].mean() * 100
+
+        expectancy = calculate_expectancy(results)
+
+        if (average_profit < 0) and (expectancy < 0):
+            expectancy = expectancy * -1
+            
+        return  (-1 * average_profit * expectancy)
