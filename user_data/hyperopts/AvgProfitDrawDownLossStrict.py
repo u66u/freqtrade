aa901ad68747334e3f82b3e4aad7392df0b01f18
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-01-16 15:27:54 +0900

    new loss

---
+++ user_data/hyperopts/AvgProfitDrawDownLossStrict.py
@@ -0,0 +1,59 @@
+"""
+MaxDrawDownHyperOptLoss
+
+This module defines the alternative HyperOptLoss class which can be used for
+Hyperoptimization.
+"""
+from datetime import datetime
+
+from pandas import DataFrame
+
+from freqtrade.constants import Config
+from freqtrade.data.metrics import calculate_expectancy, calculate_max_drawdown
+from freqtrade.optimize.hyperopt import IHyperOptLoss
+
+
+class AvgProfitDrawDownLossStrict(IHyperOptLoss):
+
+    """
+    Defines the loss function for hyperopt.
+
+    This implementation optimizes for max draw down and profit
+    Less max drawdown more profit -> Lower return value
+    """
+
+    @staticmethod
+    def hyperopt_loss_function(results: DataFrame, trade_count: int,
+                               min_date: datetime, max_date: datetime, config: Config,
+                               *args, **kwargs) -> float:
+
+        """
+        Objective function.
+
+        Uses profit ratio weighted max_drawdown when drawdown is available.
+        Otherwise directly optimizes profit ratio.
+        """
+        # total_profit = results['profit_abs'].sum()
+
+        starting_balance = config['dry_run_wallet']
+        stake_amount = config['stake_amount']
+        max_profit_abs = 0.15 * stake_amount
+
+        total_profit = min(results['profit_abs'], max_profit_abs) / starting_balance
+
+        average_profit = total_profit.mean() * 100
+
+        total_profit = min(results['profit_abs'], max_profit_abs).sum()
+
+        try:
+            max_drawdown = calculate_max_drawdown(results, value_col='profit_abs')
+        except ValueError:
+            # No losing trade, therefore no drawdown.
+            # Return 0 because this is bad scenario
+            return 0
+            # return -total_profit * 1 / trade_duration
+        
+        if (total_profit < 0) and (average_profit < 0):
+            average_profit = average_profit * -1
+
+        return  -total_profit * min(average_profit, 15) / max_drawdown[0]
