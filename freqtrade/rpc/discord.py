17437d0e1792e790346c7cce701e0f3edeff9180
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-03-17 17:20:47 +0900

    debugging

--- freqtrade/rpc/discord.py
+++ freqtrade/rpc/discord.py
@@ -36,7 +36,7 @@ class Discord(Webhook):
             msg['timeframe'] = self.timeframe
             msg['exchange'] = self._config['exchange']['name']
             # fields = self.config['discord'].get(msg['type'].value)
-            logger.info(f"Discord config: {self._config['discord'][msg['type'].value]}")
+            logger.info(f"Discord config: {self._config['discord']}")
             fields = self._config['discord'][msg['type'].value].get('rows')
             color = 0xFF6600
 
