be465333f89a3c1469ede8b1b531472dad5adeb7
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-07-22 12:17:07 +0900

    fix

--- freqtrade/data/metrics.py
+++ freqtrade/data/metrics.py
@@ -200,6 +200,10 @@ def calculate_expectancy(trades: pd.DataFrame) -> float:
     :param trades: DataFrame containing trades (requires columns close_date and profit_abs)
     :return: expectancy
     """
+
+    if len(trades) == 0:
+        return 0
+
     winning_trades = trades.loc[trades['profit_abs'] > 0]
     losing_trades = trades.loc[trades['profit_abs'] < 0]
     profit_sum = winning_trades['profit_abs'].sum()
@@ -209,8 +213,8 @@ def calculate_expectancy(trades: pd.DataFrame) -> float:
 
     average_win = (profit_sum / nb_win_trades) if nb_win_trades > 0 else 0
     average_loss = (loss_sum / nb_loss_trades) if nb_loss_trades > 0 else 0
-    winrate = nb_win_trades / len(trades)
-    loserate = nb_loss_trades / len(trades)
+    winrate = (nb_win_trades / len(trades))
+    loserate = (nb_loss_trades / len(trades))
     expectancy = (winrate * average_win) - (loserate * average_loss)
 
     return expectancy
