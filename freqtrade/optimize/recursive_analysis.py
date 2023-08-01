7dc8c51265d33acac99579653d7d9fbac524d2be
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-08-01 22:37:48 +0900

    remove squeeze

--- freqtrade/optimize/recursive_analysis.py
+++ freqtrade/optimize/recursive_analysis.py
@@ -223,10 +223,10 @@ class RecursiveAnalysis:
         restore_verbosity_for_bias_tester()
         logger.info(f"Start checking for recursive bias")
         # check and report signals
-        base_last_row = self.full_varHolder.indicators[pair_to_check].iloc[-1].squeeze()
+        base_last_row = self.full_varHolder.indicators[pair_to_check].iloc[-1]
         base_timerange = self.full_varHolder.from_dt.strftime('%Y-%m-%dT%H:%M:%S') + "-" + self.full_varHolder.to_dt.strftime('%Y-%m-%dT%H:%M:%S')
         for part in self.partial_varHolder_array:
-            part_last_row = part.indicators[pair_to_check].iloc[-1].squeeze()
+            part_last_row = part.indicators[pair_to_check].iloc[-1]
             part_timerange = part.from_dt.strftime('%Y-%m-%dT%H:%M:%S') + "-" + part.to_dt.strftime('%Y-%m-%dT%H:%M:%S')
 
             logger.info(f"Comparing last row of {base_timerange} vs {part_timerange}")
