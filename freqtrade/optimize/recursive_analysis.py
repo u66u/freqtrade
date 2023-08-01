6b4bdd29d4d40642cce91a76931436ebeb07741d
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-08-01 23:01:56 +0900

    debug

--- freqtrade/optimize/recursive_analysis.py
+++ freqtrade/optimize/recursive_analysis.py
@@ -235,8 +235,9 @@ class RecursiveAnalysis:
                 # print(compare_df)
                 for col_name, values in compare_df.items():
                     # print(col_name)
-                    print(values)
-                    logger.info(f"indicator {values[0][0]} with value {values[0][1]}")
+                    for indicator, value in values.items():
+                    # print(values)
+                        logger.info(f"indicator {indicator} with value {value}")
                     # logger.info(f"0 {col_name[0]}, 1 {col_name[1]}")
                     # col_idx = compare_df.columns.get_loc(col_name)
                     # compare_df_row = compare_df.iloc[0]
