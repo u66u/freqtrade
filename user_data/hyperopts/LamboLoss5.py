60ea7ef8fedce095dea606e36ceba794f051a168
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-09-23 08:40:00 +0900

    update lamboloss5

--- user_data/hyperopts/LamboLoss5.py
+++ user_data/hyperopts/LamboLoss5.py
@@ -11,11 +11,10 @@ from pandas import DataFrame
 from freqtrade.constants import Config
 from freqtrade.data.metrics import calculate_expectancy, calculate_max_drawdown
 from freqtrade.optimize.hyperopt import IHyperOptLoss
-import math
 
 # Set maximum expectancy used in the calculation
-max_expectancy = 1
-max_profit_ratio = 5
+max_expectancy = 10
+max_profit_ratio = 10
 max_avg_profit = 200
 
 class LamboLoss5(IHyperOptLoss):
@@ -55,6 +54,8 @@ class LamboLoss5(IHyperOptLoss):
         losing_profit = results.loc[results['profit_abs'] < 0, 'profit_abs'].sum()
         profit_factor = winning_profit / abs(losing_profit) if losing_profit else 10
 
+        nb_loss_trades = len(results.loc[results['profit_abs'] < 0])
+
         total_profit = strict_profit_abs.sum()
 
         expectancy, expectancy_ratio = calculate_expectancy(results)
@@ -62,12 +63,13 @@ class LamboLoss5(IHyperOptLoss):
         total_trades = len(results)
 
         trade_duration = results['trade_duration'].mean()
-        if trade_duration == 0:
-            trade_duration = 1
-        # if (nb_loss_trades == 0):
-        #     return -total_profit * 100
+        backtest_days = (max_date - min_date).days or 1
+        average_trades_per_day = round(total_trades / backtest_days, 5)
+
+        if (nb_loss_trades == 0):
+            return 0
         
-        loss_value = total_profit * min(average_profit, max_avg_profit) * min(profit_factor, max_profit_ratio) * min(expectancy_ratio, max_expectancy) * total_trades / math.sqrt(max(trade_duration, 5))
+        loss_value = total_profit * min(average_profit, max_avg_profit) * min(profit_factor, max_profit_ratio) * min(expectancy_ratio, max_expectancy) * average_trades_per_day
 
         if (total_profit < 0) and (loss_value > 0):
             return loss_value
